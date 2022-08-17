6b92e251f5c4cf48442934dc6c16c68ce158b11c
ockam-team <dev@ockam.io>
2022-08-17 01:13:02 +0000

    ci: update version number

--- internal/provider/constants.go
+++ internal/provider/constants.go
@@ -4,7 +4,7 @@ package provider
 // for release automation to work
 
 const (
-	version = "0.69.0"
+	version = "0.70.0"
 	baseURL = "https://github.com/ockam-network/ockam/releases/download/ockam_v%s/ockam.%s-%s"
 	binary  = "ockam-v" + version
 	arm64   = "arm64"
